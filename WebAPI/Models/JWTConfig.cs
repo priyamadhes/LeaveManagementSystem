e16b168968815223b093c8c487cb8d033b9f35f8
priyamadhes <49122084+priyamadhes@users.noreply.github.com>
2021-08-22 01:41:55 +0400

    Add files via upload

---
+++ WebAPI/Models/JWTConfig.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LmsWebApi.Models
+{
+    public class JWTConfig
+    {
+        public string Key { get; set; }
+
+        public string Issuer { get; set; }
+
+        public string Audience { get; set; }
+
+    }
+}
---
+++ WebAPI/Models/ResponseModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using LmsWebApi.Enums;
+
+namespace LmsWebApi.Models
+{
+    public class ResponseModel
+    {
+        public ResponseModel(ResponseCode responsecode,string responseMessage,object dataSet)
+        {
+            ResponseCode = responsecode;
+            ResponseMessage = responseMessage;
+            Dataset = dataSet;
+        }
+        public ResponseCode ResponseCode { get; set; }
+
+        public string ResponseMessage { get; set; }
+
+        public object Dataset { get; set; }
+
+
+
+    }
+}
