bfdcd25cad797d76e3c99ea188f82dd8ba466675
priyamadhes <49122084+priyamadhes@users.noreply.github.com>
2021-08-22 01:40:38 +0400

    Add files via upload

---
+++ WebAPI/Models/BindingModel/LeaveModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LmsWebApi.Models.BindingModel
+{
+    public class LeaveModel
+    {
+        [Required]
+        public string Email { get; set; }
+       
+        public string FromDate { get; set; }
+        public string ToDate { get; set; }
+        public string LeaveType { get; set; }
+        public string LeaveDetail { get; set; }
+    }
+}
