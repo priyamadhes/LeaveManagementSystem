bf238536c3a6f895ba9e64466809e48e15ce92ec
priyamadhes <49122084+priyamadhes@users.noreply.github.com>
2021-08-22 01:41:24 +0400

    Add files via upload

---
+++ WebAPI/Models/DTO/LeaveDTO.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LmsWebApi.Models.DTO
+{
+    public class LeaveDTO
+    {
+        //public LeaveDTO(string fullname, string email, DateTime FromDate, DateTime ToDate,string LeaveType,string LeaveDetail)
+        //{
+        //    FullName = fullname;
+        //    Email = email;
+        //    UserName = username;
+        //    DateCreated = dateCreated;
+        //}
+        //public string FullName { get; set; }
+        //public string Email { get; set; }
+        //public string UserName { get; set; }
+        //public DateTime DateCreated { get; set; }
+        //public string Token { get; set; }
+    }
+}
