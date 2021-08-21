2a308fa86375875d147939d4b5b1220e2b60841d
priyamadhes <49122084+priyamadhes@users.noreply.github.com>
2021-08-22 01:37:13 +0400

    Add files via upload

---
+++ WebAPI/Enums/ResponseCode.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LmsWebApi.Enums
+{
+    public enum ResponseCode
+    {
+        Notset=0,
+        Ok=1,
+        Error=2
+    }
+}
